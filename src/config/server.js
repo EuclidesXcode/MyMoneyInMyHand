8d1a8c19fcf4a9809b5d7250dbb22e51cf3fa92d
EuclidesNascimento <euclides.nascimento@vivitech.com.br>
2020-07-26 02:42:39 -0300

    backend done

--- src/config/server.js
+++ src/config/server.js
@@ -2,12 +2,14 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 app.use(allowCors)
+app.use(queryParser())
 
 app.listen(port, function() {
     console.log(`API Online na porta ${port}`)
