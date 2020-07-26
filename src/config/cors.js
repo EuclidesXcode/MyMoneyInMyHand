cfd2b87570607dc576e23e5a777401292ff4c321
EuclidesNascimento <euclides.nascimento@vivitech.com.br>
2020-07-26 02:35:06 -0300

    Add cors

---
+++ src/config/cors.js
@@ -0,0 +1,5 @@
+module.exports = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-headers', 'Origin, X-Requested-With, Content-Type, Accept')
+}
--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const allowCors = require('./cors')
 
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
+app.use(allowCors)
 
 app.listen(port, function() {
     console.log(`API Online na porta ${port}`)
